bf62a9754285240d34f4ab5285e89f2a989515e8
webby813 <webbyteo02@gmail.com>
2026-05-10 16:42:00 +0800

    Assemble chassis and wire L298N: add motor test sketch
    Motors and wheels mounted; L298N wired to the Uno (IN1-IN4, ENA).
    Test sketch spins both motors forward for one second after boot.
    
    Known issue: the boards boot from USB but not from the 2x1.5V battery
    pack - supply voltage looks too low.

---
+++ motor_test/motor_test.ino
@@ -0,0 +1,37 @@
+int IN1 = 10;
+int IN2 = 9;
+int IN3 = 12;
+int IN4 = 11;
+int ENA = 6; //Turn motor on off
+
+void setup() {
+  testingRun();
+}
+
+void loop() {
+}
+
+void testingRun(){
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+
+  // Left motor forward
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+
+  // Right motor forward
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  delay(1000);
+
+  // Stop all motors
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  digitalWrite(ENA, LOW);
+}
